a9d46e63c434f31d157d6c613caaab9b395294df
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-12-14 17:04:24 -0800

    tests are written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,12 +2,50 @@ const request = require('supertest');
 const server = require('../lib/app');
 
 describe('app routes', () => {
+  
+  
   it.only('responds', async() => {
-    const response = await request(server)
-      .get('/blue');
-        
-    expect(response.body).toEqual({
-      name: 'hello'
-    });
+    const res = await request(server)
+      .get('/');
+ 
+    expect(res.body).toequal('name: hello');
+  
   });
+
+  // ------------------------------------------
+
+  it('returns red', async() => {
+    const res = await request(server)
+      .get('/redrum');
+
+    expect(res.body).toEqual(
+      '<html><body><h1>REDRUM</h1></body></html>'
+    );
+  });
+
+  // ------------------------------------------
+
+  it('returns green', async() => {
+    const res = await request(server)
+      .get('/greenyum');
+
+    expect(res.body).toEqual(
+      '<html><body><h1>GREEN BEANS</h1></body></html>'
+    );
+  });
+
+  // ------------------------------------------
+
+  it('returns blue', async() => {
+    const res = await request(server)
+      .get('/bluegoo');
+
+    expect(res.body).toEqual(
+      '<html><body><h1>OCEAN SPRAY</h1></body></html>'
+    );
+  });
+
+  // ------------------------------------------
+
+
 });
