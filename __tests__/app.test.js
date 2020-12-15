df0551a9ad93debd511a496a86a92d1c79204f9d
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-12-14 16:43:29 -0800

    simple testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,13 @@
 const request = require('supertest');
-const app = require('../lib/app');
+const server = require('../lib/app');
 
 describe('app routes', () => {
-
+  it.only('responds', async() => {
+    const response = await request(server)
+      .get('/blue');
+        
+    expect(response.body).toEqual({
+      name: 'hello'
+    });
+  });
 });
